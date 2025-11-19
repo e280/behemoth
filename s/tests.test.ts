f6defdbe4c45e8ee64cdecba5ca70bee08b3536f
Chase Moskal <chasemoskal@gmail.com>
2025-11-19 00:00:34 -0800

    add: real tests

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,18 +1,31 @@
 
-import {BehemothDisk} from "./node/behemoth-disk.js"
+import {bytes} from "@e280/stz"
+import fs from "node:fs/promises"
 import {Science, suite, test, expect} from "@e280/science"
+import {BehemothDisk} from "./node/behemoth-disk.js"
+import {BehemothMemory} from "./core/behemoth-memory.js"
 
 await Science.run({
-	"node": suite({
-		"cycle": test(async() => {
-			// const behemoth = await BehemothDisk.make("./x/example")
-			// const bytes = new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF])
-			//
-			// const hash = await behemoth.set(new Blob([bytes]))
-			// console.log("saved", hash)
-			//
-			// const data = await behemoth.get(hash)
-			// console.log("loaded", data?.size)
+	"BehemothMemory": suite({
+		"set+get": test(async() => {
+			const behemoth = new BehemothMemory()
+			const alpha = new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF])
+			const hash = await behemoth.set(new Blob([alpha]))
+			const bravo1 = await behemoth.require(hash)
+			const bravo2 = new Uint8Array(await bravo1.arrayBuffer())
+			expect(bytes.eq(alpha, bravo2)).is(true)
+		}),
+	}),
+
+	"BehemothDisk (node)": suite({
+		"set+get": test(async() => {
+			const behemoth = await BehemothDisk.make("./yard")
+			const alpha = new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF])
+			const hash = await behemoth.set(new Blob([alpha]))
+			const bravo1 = await behemoth.require(hash)
+			const bravo2 = new Uint8Array(await bravo1.arrayBuffer())
+			expect(bytes.eq(alpha, bravo2)).is(true)
+			await fs.rm(`./yard/${hash}`)
 		}),
 	}),
 })
