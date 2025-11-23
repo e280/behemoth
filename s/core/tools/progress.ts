f4840fa7c667056300b671afb6d5e61e5d4ee867
Chase Moskal <chasemoskal@gmail.com>
2025-11-23 11:10:36 -0800

    tidy: syntax

--- s/core/tools/progress.ts
+++ s/core/tools/progress.ts
@@ -2,7 +2,11 @@
 import {Progress} from "../types.js"
 import {rateLimit} from "./rate-limit.js"
 
-export function progress(total: number, onProgress: ((p: Progress) => void) = (() => {})) {
+export function progress(
+		total: number,
+		onProgress: ((p: Progress) => void) = (() => {}),
+	) {
+
 	return rateLimit(100, (done: number) => onProgress({total, done}))
 }
 
